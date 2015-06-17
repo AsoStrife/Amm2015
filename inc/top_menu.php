218f8e5bb800f86d473a2c63c5eb1246493084a7
Andrea <me@andreacorriga.com>
2015-06-17 16:40:12 +0200

    Bugfix

--- inc/top_menu.php
+++ inc/top_menu.php
@@ -1,6 +1,6 @@
 <div class="content">
 	<div style="float:left; width:500px;">
-    	<a href="/"><img src="img/ffstory-logo.png" class="logo"/></a>
+    	<a href="index.php"><img src="img/ffstory-logo.png" class="logo"/></a>
     </div> 
     <ul id="menu_css_orizzontale">
         <li><a href="index.php" <? if($page == 'index.php') echo'class="active"'; ?> >Home</a></li>
